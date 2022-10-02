fd8d1dd4519d1ee572427d87b3583e356148101d
Manuel Šarić <manujelko@gmail.com>
2022-10-02 18:28:19 +0200

    Return books

---
+++ books.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = {
+    "book_1": {"title": "Title One", "author": "Author One"},
+    "book_2": {"title": "Title Two", "author": "Author Two"},
+    "book_3": {"title": "Title Three", "author": "Author Three"},
+    "book_4": {"title": "Title Four", "author": "Author Four"},
+    "book_5": {"title": "Title Five", "author": "Author Five"},
+}
+
+
+@app.get("/")
+async def read_all_books():
+    return BOOKS
